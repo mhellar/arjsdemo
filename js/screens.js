0e53c0a45514523098700f165d8c34582f41dea1
Francis Li <mail@francisli.com>
2018-11-04 11:39:07 -0800

    Screen scaling

--- js/screens.js
+++ js/screens.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  let width = $(window).width();
+  let height = $(window).height();
+  let ratio = Math.min(width / 375, height / 667);
+  console.log('width', width, 'height', height, 'ratio', ratio);
+  $('#container').css('transform', `scale(${ratio})`);
   setTimeout(function() {
     // window.location = 'index8.html';
     $('#splash').hide();
